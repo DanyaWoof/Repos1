316f82a8ce52de4b7a0c60f20e8890691c77bac1
DanyaRaboch <smartden777@gmail.com>
2022-07-26 16:46:42 +0300

    js operators

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let admin ='admiral';
+let name = 'jonh';
+admin=name;
+
+let boolVal = true;
+alert(typeof boolVal);
+boolVal = String(boolVal);
+alert(typeof(boolVal));
+// alert(confirm('Are boobs bestest part of a woman body?'))
+// alert(admin);
+// result = prompt('china number', '2');
+// alert(result)
+
+
+
+
