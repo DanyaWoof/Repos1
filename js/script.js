5672da2321f60fb49052450eac2f8f6daca238f1
DanyaRaboch <smartden777@gmail.com>
2022-07-28 16:51:09 +0300

    cicles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,51 @@
-let age = prompt('Age','1');
-if (+age < 14 || +age > 90) {
-    alert ('norm');    
-}
-else {
-    alert('nenorm');
-}
+//
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+// //simple numbers
+// let n = '';
+// let cnt = 0;
+// joper:for (let i= 2; i<= 10; i++){
+//     for (let j= 2; j<= i; j++){
+//         if(i == j) {
+//             n = n + String(i) + '; ';   
+//             cnt++;
+//         }
+//         else if (i % j == 0)
+//         continue joper;
+//     }
+// }
+// alert (n);
+// alert (cnt);
+//----------------------------------------------------------------------------------------------------
+// let inputNum;
+
+// while(true){
+//     inputNum = prompt('text bigger than 100','');
+//     if (inputNum >=100 || inputNum == null)
+//     break;
+// }
+//----------------------------------------------------------------------------------------------------
+// let login = prompt('Login','');
+// let pwrd;
+// let message = '000';
+
+// if (login === 'admin') {
+//     pwrd = prompt('PassWord?','im the head');
+//     message = (pwrd === 'im the head')? 'Salamalekum' : (pwrd == null)? 'Aborted' : 'WrongPass';
+// }
+// else {
+//     message = (login == null)? 'Aborted' : 'Who are you?'; 
+// }
+// alert(message);
